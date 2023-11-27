2a6e805c53eabe72cb720bb2306b6d164a040737
beykyle <beykyle@umich.edu>
2023-11-27 11:59:18 -1000

    add sorting and sanitizing of points to bsp

--- src/osiris/bsp.py
+++ src/osiris/bsp.py
@@ -4,12 +4,39 @@ import numpy as np
 
 class BinarySPTree:
     def __init__(self, depth, lower_bound, upper_bound, data):
+        self.depth = depth
+        self.size = 2**depth
         self.data = data
+        self.dimensions = lower_bound.size
         idx = np.arange(0, len(data))
+
+        # first sanitizing bounds to freeze parameters that are equal
+        mask = lower_bound == upper_bound
+        self.frozen_param_idxs = np.nonzero(mask)
+        self.frozen_params = lower_bound[mask]
+        n_unfrozen = lower_bound[np.logical_not(mask)].size
+        self.active_dimensions = n_unfrozen
+
+        # bounds for unfrozen params only
+        self.param_mask = np.logical_not(mask)
+        self.frozen_mask = mask
+        lower_bound = lower_bound[self.param_mask]
+        upper_bound = upper_bound[self.param_mask]
+
         self.bsp = BSPtree(depth, lower_bound, upper_bound, idx)
 
     def idx(self, point):
-        return self.bsp.at(point)
+        assert np.all(point[self.frozen_mask] == self.frozen_params)
+        return self.bsp.at(point[self.param_mask])
 
     def at(self, point):
-        return self.data[self.bsp.at(point)]
+        assert np.all(point[self.frozen_mask] == self.frozen_params)
+        return self.data[self.bsp.at(point[self.param_mask])]
+
+    def sort(self, points):
+        out = [[] for i in range(self.size)]
+        for point in points:
+            idx = self.idx(point)
+            out[idx].append(point)
+
+        return out
--- tests/test_bsp.py
+++ tests/test_bsp.py
@@ -49,14 +49,59 @@ class BSPTest(TestCase):
             np.array(["a", "b", "c", "d"], dtype=object),
         )
 
-        np.testing.assert_equal(bsp.at(np.array([-1.0 / 2, -1.0 / 2, -1.0 / 2])), "a")
-        np.testing.assert_equal(bsp.at(np.array([-1.0 / 2, -1.0 / 2, 1.0 / 2])), "a")
+        np.testing.assert_string_equal(bsp.at(np.array([-1.0 / 2, -1.0 / 2, -1.0 / 2])), "a")
+        np.testing.assert_string_equal(bsp.at(np.array([-1.0 / 2, -1.0 / 2, 1.0 / 2])), "a")
 
-        np.testing.assert_equal(bsp.at(np.array([-1.0 / 2, 1.0 / 2, -1.0 / 2])), "b")
-        np.testing.assert_equal(bsp.at(np.array([-1.0 / 2, 1.0 / 2, 1.0 / 2])), "b")
+        np.testing.assert_string_equal(bsp.at(np.array([-1.0 / 2, 1.0 / 2, -1.0 / 2])), "b")
+        np.testing.assert_string_equal(bsp.at(np.array([-1.0 / 2, 1.0 / 2, 1.0 / 2])), "b")
 
-        np.testing.assert_equal(bsp.at(np.array([1.0 / 2, -1.0 / 2, -1.0 / 2])), "c")
-        np.testing.assert_equal(bsp.at(np.array([1.0 / 2, -1.0 / 2, 1.0 / 2])), "c")
+        np.testing.assert_string_equal(bsp.at(np.array([1.0 / 2, -1.0 / 2, -1.0 / 2])), "c")
+        np.testing.assert_string_equal(bsp.at(np.array([1.0 / 2, -1.0 / 2, 1.0 / 2])), "c")
 
-        np.testing.assert_equal(bsp.at(np.array([1.0 / 2, 1.0 / 2, -1.0 / 2])), "d")
-        np.testing.assert_equal(bsp.at(np.array([1.0 / 2, 1.0 / 2, 1.0 / 2])), "d")
+        np.testing.assert_string_equal(bsp.at(np.array([1.0 / 2, 1.0 / 2, -1.0 / 2])), "d")
+        np.testing.assert_string_equal(bsp.at(np.array([1.0 / 2, 1.0 / 2, 1.0 / 2])), "d")
+
+
+    def test_bsp_equal_bounds(self):
+
+        # This bsp is only 2D; the last dimension will be ignored bc the upper and lower
+        # bounds are equal.
+        # This means we split x at 0, then y at 0, then x at +/- 1/2
+        # divides 2D box into 8 sub-boxes using planes y=0 and x=0, x=-1/2, x=+1/2
+        #
+        #     y
+        #     |
+        #   1 -------------
+        #     |2 |3 |6 |7 |
+        #   0 -------------
+        #     |0 |1 |4 |5 |
+        #  -1 ---------------x
+        #    -1 -1/2  1/2  1
+        #
+        bsp = osiris.BinarySPTree(
+            3,
+            np.array([-1, -1, 1]),
+            np.array([1, 1, 1]),
+            np.arange(0, 8, 1),
+        )
+
+        np.testing.assert_equal(bsp.at(np.array([-0.75, -0.75, 1])), 0)
+        np.testing.assert_equal(bsp.at(np.array([-0.75, -0.75, 1])), 0)
+
+        np.testing.assert_equal(bsp.at(np.array([0.75, 0.75, 1])), 7)
+        np.testing.assert_equal(bsp.at(np.array([0.75, 0.75, 1])), 7)
+
+
+    def test_sort(self):
+        bsp = osiris.BinarySPTree(
+            2, np.array([-1, -1, -1]), np.array([1, 1, 1]), np.array([0, 1, 2, 3])
+        )
+
+        params = np.array([[-0.5, -0.5, -0.5], [0.9, 0.9, 0.9], [0.8, 0.8, 0.8]])
+        sorted_params = bsp.sort(params)
+
+        np.testing.assert_equal(sorted_params[0] , [np.array([-0.5, -0.5, -0.5])])
+        np.testing.assert_equal(sorted_params[1] , [])
+        np.testing.assert_equal(sorted_params[2] , [])
+        np.testing.assert_equal(sorted_params[3] , [
+            np.array([0.9, 0.9, 0.9]), np.array([0.8, 0.8, 0.8])])
